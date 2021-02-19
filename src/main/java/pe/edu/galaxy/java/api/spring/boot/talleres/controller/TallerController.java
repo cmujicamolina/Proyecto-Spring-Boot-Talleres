cc2086b70d961c5fdc1e5a08aafc29496e636c3b
Christian Mujica <christianedwinmm@gmail.com>
2021-02-19 17:36:54 -0500

    Actualiza version3

--- src/main/java/pe/edu/galaxy/java/api/spring/boot/talleres/controller/TallerController.java
+++ src/main/java/pe/edu/galaxy/java/api/spring/boot/talleres/controller/TallerController.java
@@ -2,7 +2,10 @@ package pe.edu.galaxy.java.api.spring.boot.talleres.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.Data;
+import org.springframework.web.server.ResponseStatusException;
+import lombok.extern.slf4j.Slf4j;
 import pe.edu.galaxy.java.api.spring.boot.talleres.model.Taller;
 import pe.edu.galaxy.java.api.spring.boot.talleres.service.TallerService;
 
+@Slf4j
 @RestController
 @RequestMapping("/talleres/v1")
 public class TallerController {	
@@ -24,11 +29,28 @@ public class TallerController {
 	private TallerService tallerService;
 	
 	@GetMapping("/{id}")
-	public Taller getTaller(@PathVariable Long id) {
+	public ResponseEntity<Taller> getTaller(@PathVariable Long id) {
+		
 		if (id<=0) {
-			
+			return ResponseEntity.badRequest().build();
 		}
-		return this.tallerService.getTaller(id);
+		Taller taller=this.tallerService.getTaller(id);
+		if (taller==null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(taller);
+	}
+	
+	@GetMapping("/{anio}/nroExp")
+	public ResponseEntity<Taller> getExpediente(@PathVariable Short anio,@PathVariable String nroExp) {
+		return null;
+	}
+	
+	@GetMapping("/buscarReclamos")
+	public ResponseEntity<Taller> getReclamos(@RequestParam Short anio,
+			@RequestParam String nroExp,
+			@RequestParam String resumen) {
+		return null;
 	}
 	
 	@GetMapping
@@ -37,19 +59,33 @@ public class TallerController {
 		if(lst.isEmpty()) {
 			return ResponseEntity.noContent().build();
 		}
-		return ResponseEntity.ok(lst);	
+		return ResponseEntity.ok(lst);		
 	}
 	
 	@GetMapping
 	@RequestMapping("/buscar")
-	public List<Taller> getTalleres(@RequestParam String nombre) {
+	public ResponseEntity<List<Taller>> getTalleres(@RequestParam String nombre) {
 		Taller taller= Taller.builder().nombre(nombre).build();
-		return this.tallerService.buscar(taller);		
+		List<Taller> lst=this.tallerService.buscar(taller);	
+		if(lst.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(lst);	
 	}
 	
 	@PostMapping
-	public Taller insertar(@RequestBody Taller taller) {
-		return this.tallerService.grabar(taller);		
+	public ResponseEntity<Taller> insertar(@Validated  @RequestBody Taller taller, BindingResult result) {
+		if (result.hasErrors()) {
+			String msgErr= result.getAllErrors().toString();
+			log.error("msgErr" +msgErr);
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,msgErr);
+		}
+		
+		Taller oTaller=tallerService.grabar(taller);
+		if (oTaller!=null) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(oTaller);
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();	
 	}
 	
 	@PutMapping("/{id}")
--- src/main/java/pe/edu/galaxy/java/api/spring/boot/talleres/model/Taller.java
+++ src/main/java/pe/edu/galaxy/java/api/spring/boot/talleres/model/Taller.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,13 +28,17 @@ public class Taller {
 	@Builder.Default
 	private Long id=0L;
 	
-	@Column(name="NOMBRE")
+	@Size(min=5, max=220, message="El nombre  es requerido y debe ser mayor que 5 y máximo 220 carácteres")
+	@Column(name="NOMBRE", length=220,nullable=false)
 	private String nombre;
 	
+	@Size(min=10, max=280, message="La descripción es requerida y debe ser mayor que 10 y máximo 280 carácteres")
 	@Column(name="DESCRIPCION")
 	private String descripcion;
 	
+	@Size(min=1, max=1, message="El estado es requerido y debe ser 0 o 1")
 	@Column(name="ESTADO")
-	private String estado;
+	@Builder.Default
+	private String estado="1";
 
 }
